8d2e84a11c8269de3dc9082bb445a154eb947971
Developer 1 <developer1@interopay.com>
2025-09-07 21:59:02 +0530

    Create patterns.js
    star pattern created

---
+++ javascript-questions/patterns.js
@@ -0,0 +1,37 @@
+// let sum = 0;
+//for ( let x = 0; x<=10; x++){
+   // sum+=x
+   // console.log(sum)
+ //}
+ 
+
+//for (let x = 1; x<=10; x++){
+  //  console.log (`5 x ${x} ${5*x}`) 
+  //}
+ // for ( let x = 1 ; x<=10; x++ ){
+  //  console.log (`6 x ${x} ${6*x}`)
+  //}
+
+   /*for (let x= 1; x<=10; x++){
+    console.log ( `7 x ${x} =${7*x}`)
+   }*/
+    
+   //for (let x = 0; x<=10; x++){
+    //const y = x
+    //console.log (y)
+   //}
+   //for(let x= 0; x<=20;x++){
+     //const y = x
+     //console.log(y)
+ //}
+
+ let n= 5
+ for(let i=1;i<=5;i++){
+     let x="  ".repeat(n-i) + "* ".repeat(i);
+console.log (x);
+ }
+ for(let i=n;i>=1;i--){
+     let x="  ".repeat(n-i) + "* ".repeat(i);
+     console.log (x);
+ } 
+    
